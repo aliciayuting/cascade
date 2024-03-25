5083db7620b188dd51b5ba14295cc8f04f8dba74
aliciayuting <yy354@cornell.edu>
2024-03-25 17:19:05 -0400

    fuse test with fd

--- src/service/fuse/fuse_perftest.cpp
+++ src/service/fuse/fuse_perftest.cpp
@@ -1,114 +1,153 @@
+#include <cascade/service_types.hpp>
+#include <derecho/utils/logger.hpp>
+
 #include <fstream>
 #include <iostream>
 #include <chrono>
 #include <vector>
 #include <filesystem> 
 #include <string>
+#include <cstdlib>
+#include <unistd.h>
+#include <cstdint>  
+#include <cstring> 
 
-void createFile(const std::filesystem::path& filePath, size_t fileSize) {
-    std::ofstream file(filePath, std::ios::binary | std::ios::trunc);
-    if (!file) {
-        std::cerr << "File could not be opened for writing at " << filePath << ".\n";
-        exit(1);
+using namespace derecho::cascade;
+
+#define READ_START_TIME 1000
+#define READ_END_TIME 1004
+
+void record_timestamp(std::vector<std::chrono::nanoseconds> timestamps, std::string& file_name, uint32_t file_size, uint32_t runs) {
+    std::ofstream perf_file("perf/" + file_name, std::ios::binary);
+    int64_t total_time = 0;
+    for (size_t i = 0; i < timestamps.size(); i++) {
+        perf_file << "Run " << i + 1 << " : " << timestamps[i].count() << " ns\n";
+        total_time += timestamps[i].count();
     }
-    const std::string data = "cascade";
-    size_t written = 0;
-    while (written < fileSize) {
-        file << data;
-        written += data.size();
-    }
-    file.close();  
+    double average_time = total_time / runs;
+    perf_file << "Average time to write " << file_name 
+              << " over " << runs << " runs: " << average_time << " ns"
+              << " \nFile size: " << file_size / 1024 << " KB\n";
+    perf_file.close();
 }
 
-bool verifyContent(const std::vector<char>& buffer, const std::string& expectedWord) {
-    std::string bufferContent(buffer.begin(), buffer.end());
-    std::cout << bufferContent << std::endl;
-    size_t pos = 0;
-    while (pos < bufferContent.size()) {
-        size_t nextPos = bufferContent.find(expectedWord, pos);
-        if (nextPos != pos) {
-            return false;
+void read_test(uint32_t file_size, uint32_t runs, const std::filesystem::path& path, bool verify) {
+    for (int i = 1; i <= runs; ++i) {
+        std::string file_name = "read_test" + std::to_string(i);
+        std::filesystem::path read_path = path / file_name;
+        // std::ifstream file(read_path, std::ios::binary);
+        // if (!file) {
+        //     std::cerr << "File could not be opened for reading at " << read_path << ".\n";
+        //     return;
+        // }
+        // char *buffer = static_cast<char*>(malloc(file_size + 1));
+        // TimestampLogger::log(READ_START_TIME,4,i,get_walltime());
+        // if (!file.read(buffer, file_size)) {
+        //     std::cerr << "Error reading file on run " << i + 1 << ".\n";
+        //     file.close();
+        //     return;
+        // }
+        // file.close();
+        // TimestampLogger::log(READ_END_TIME,4,i,get_walltime());
+        // if (verify && i == runs - 1) {
+        //     std::ofstream verify_file("perf/read_verify.txt", std::ios::binary);
+        //     verify_file.write(reinterpret_cast<const char*>(buffer), file_size);
+        //     verify_file.close();
+        // }
+        // usleep(10000);
+
+        // Open the file for reading (O_RDONLY)
+        int fd = open(read_path.c_str(), O_RDONLY);
+        if (fd == -1) {
+            std::cerr << "Error opening file: " << strerror(errno) << std::endl;
+            return;
         }
-        pos = nextPos + expectedWord.size();
+        // Buffer to store file contents
+        constexpr size_t bufferSize = 1024;
+        char buffer[bufferSize];
+
+        // Read up to bufferSize bytes from the file
+        ssize_t bytesRead = read(fd, buffer, bufferSize - 1); // leave space for null terminator
+
+        if (bytesRead == -1) {
+            std::cerr << "Error reading file: " << strerror(errno) << std::endl;
+            close(fd); // Make sure to close the file descriptor even if read fails
+            return;
+        }
+        // Null-terminate the string
+        buffer[bytesRead] = '\0';
+        // Close the file
+        if (close(fd) == -1) {
+            std::cerr << "Error closing file: " << strerror(errno) << std::endl;
+            return;
+        }
+        // Print the contents read from the file
+        std::cout << "File contents:\n" << buffer << std::endl;
     }
-    return true; 
+    std::string logger_path = "fuse_perftest_logger.csv"; 
+    TimestampLogger::flush(logger_path, false);
 }
 
-void read_test(uint32_t kb_size, uint32_t runs, const std::filesystem::path& path) {
-    using namespace std::chrono;
-    const std::filesystem::path filePath = path / "read_file.txt";
-    std::filesystem::create_directories(path);
-
-    std::vector<double> timings;
-    size_t fileSize = kb_size * 1024; 
-    createFile(filePath, fileSize);
-    for (int i = 0; i < runs; ++i) {
-        std::ifstream file(filePath, std::ios::binary);
+// TODO (chuhan) : add logger for write tests
+void write_test(uint32_t file_size, uint32_t runs, const std::filesystem::path& path, bool verify) {
+    std::filesystem::path write_path = path / "write_test";
+    char *buffer = static_cast<char*>(malloc(file_size + 1));
+    for (size_t i = 0; i < file_size; i++) {
+        buffer[i] = '1';
+    }
+    std::vector<std::chrono::nanoseconds> timestamps;
+    for (int i = 1; i <= runs; ++i) {
+        std::ofstream file(write_path, std::ios::binary);
         if (!file) {
-            std::cerr << "File could not be opened for reading at " << filePath << ".\n";
+            std::cerr << "File could not be opened at " << write_path << ".\n";
         }
-        std::vector<char> buffer(fileSize);
-        auto start = high_resolution_clock::now();
-        if (!file.read(buffer.data(), fileSize)) {
-            std::cerr << "Error reading file on run " << i+1 << ".\n";
+        TimestampLogger::log(READ_START_TIME,4,i,get_walltime());
+        if (!file.write(buffer, file_size)) {
+            std::cerr << "Error writing file on run " << i + 1 << ".\n";
             file.close();
-            continue;
-        }
-        auto end = high_resolution_clock::now();
-        file.close();
-        if (i >= 1000) {
-            timings.push_back(duration<double>(end - start).count());
-        }
-    }
-    double averageTime = 0;
-    for (auto time : timings) {
-        averageTime += time;
-    }
-    averageTime /= (runs - 1000);
-    std::cout << "Average time to read a newly created file " << filePath 
-              << " over " << runs << " runs: " << averageTime 
-              << " s\nFile size: " << kb_size << " KB\n";
-}
-
-void write_test(uint32_t kb_size, uint32_t runs, const std::filesystem::path& path) {
-    using namespace std::chrono;
-    const std::string word = "cascade";
-    const size_t wordSize = word.size();
-    const size_t fileSize = kb_size * 1024;
-    const size_t repetitions = fileSize / wordSize;
-
-    std::vector<double> timings;
-    std::ofstream file;
-    std::filesystem::create_directories(path);
-    const std::filesystem::path filePath = path / "benchmark_file.txt";
-    for (int i = 0; i < runs; ++i) {
-        file.open(filePath, std::ios::binary | std::ios::trunc);
-        if (!file) {
-            std::cerr << "File could not be opened at " << filePath << ".\n";
-        }
-        auto start = high_resolution_clock::now();
-        for (size_t j = 0; j < repetitions; ++j) {
-            file << word;
+            return;
         }
         file.close();
-        auto end = high_resolution_clock::now();
-        if (i >= 1000) {
-            timings.push_back(duration<double>(end - start).count());
-        }
+        TimestampLogger::log(READ_END_TIME,4,i,get_walltime());
+        usleep(10000);
     }
-    double averageTime = 0;
-    for (auto time : timings) {
-        averageTime += time;
-    }
-    averageTime /= (runs - 1000);
-    std::cout << "Average time to write to " << filePath 
-              << " over " << runs << " runs: " << averageTime 
-              << " s\nFile size: " << kb_size << " KB\n";
+    std::string file_name = "write_perf_" + std::to_string(file_size / 1024) + "_kb_" + std::to_string(runs) + "_runs_perftest.csv"; 
+    TimestampLogger::flush(file_name, false);
 }
 
-int main() {
-    std::filesystem::path objp_path = "test/latest/pool1";
-    // write_test(1, 100000, objp_path);
-    read_test(1000, 1, objp_path);
+int main(int argc, char* argv[]) {
+    if (argc < 5) {
+        std::cerr << "Usage: " << argv[0] << " <read/write> <-s> <file size in KB> <-n> <num runs> <-v> (optional)\n";
+        return 1;
+    }
+    std::string operation = argv[1];  
+    if (operation != "read" && operation != "write") {
+        std::cerr << "Invalid operation. Please use 'read' or 'write'.\n";
+        return 1; 
+    }
+    uint32_t kb_size, num_runs;
+    bool verify = false;
+    for (int i = 1; i < argc; i++) {
+        std::string arg = argv[i];
+        if (arg == "-s" && i + 1 < argc) {
+            kb_size = std::atoi(argv[++i]);
+        } else if (arg == "-n" && i + 1 < argc) {
+            num_runs = std::atoi(argv[++i]);
+        } else if (arg == "-v") {
+            verify = true;
+        }
+    }
+    uint32_t file_size = kb_size * 1024; 
+    std::filesystem::path objp_path = "test/latest/pool";
+    std::cout << "Operation: " << operation << std::endl;
+    std::cout << "File size: " << kb_size << " KB" << std::endl;
+    std::cout << "Number of runs: " << num_runs << std::endl;
+
+    std::filesystem::create_directories("perf");
+    if (operation == "read") {
+        read_test(file_size, num_runs, objp_path, verify);
+    } else if (operation == "write") {
+        write_test(file_size, num_runs, objp_path, verify);
+    }
     return 0;
 }
